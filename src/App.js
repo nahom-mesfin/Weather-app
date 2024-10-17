6dd4fd52cd520167bf5a47c9ae2b32f71053c7b7
nahom-mesfin <nahommesfin116@gmail.com>
2024-10-17 17:58:34 -0300

    some structure

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import axios from "axios";
+import React, { useState } from "react";
+
 function App() {
+  //const url = "https://api.openweathermap.org/data/2.5/weather?lat=9.0192&lon=38.7525&appid=06aa447f4c130cdeaa572a966e9fd761";
+
   return (
-    <div className="App">
-      <h1>Weather App</h1>
+    <div className="app">
+      <div className="container">
+        <div className="top">
+          <div className="location">
+            <p>Dallas</p>
+          </div>
+          <div className="bottom"></div>
+        </div>
+      </div>
     </div>
   );
 }
