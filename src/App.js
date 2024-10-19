06042509754a4c6ec508eb6ecefb43d577d661d5
nahom-mesfin <nahommesfin116@gmail.com>
2024-10-19 16:50:33 -0300

    basic css

--- src/App.js
+++ src/App.js
@@ -9,9 +9,23 @@ function App() {
       <div className="container">
         <div className="top">
           <div className="location">
-            <p>Dallas</p>
+            <p>
+              <h1>Dallas</h1>
+            </p>
           </div>
-          <div className="bottom"></div>
+          <div className="temp">
+            <h1> 65° F</h1>
+          </div>
+          <div className="description">Clouds</div>
+          <div className="bottom">
+            <div className="feels">
+              <p>65° F</p>
+            </div>
+            <div className="himuduty">
+              <p>20%</p>
+            </div>
+            <div className="wind">12 MPH</div>
+          </div>
         </div>
       </div>
     </div>
